9690ba9b876520a0ef2e2ed04794a93b63fd7e40
Uche Ngadi <uchengadi@yahoo.com>
2022-10-17 16:43:37 +0100

    redirect api url

--- src/apis/local.js
+++ src/apis/local.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:9000/api/v1", //for the localhost
+  //baseURL: "http://localhost:9000/api/v1", //for the localhost
   //baseURL: "https://api.brightwayafrica.com/api/v1", // for production
+  baseURL: "http://3.225.222.48/api/v1", // for production
 });
--- src/apis/util.js
+++ src/apis/util.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:9000/";
-//export const baseURL = "https://api.brightwayafrica.com";
+//export const baseURL = "http://localhost:9000/";
+export const baseURL = "http://3.225.222.48/";
