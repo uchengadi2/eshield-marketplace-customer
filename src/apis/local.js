4c06b3fcc7e3459f85c2a2b8316dce36357c6087
Uche Ngadi <uchengadi@yahoo.com>
2022-10-17 16:58:11 +0100

    api url redirection 2

--- src/apis/local.js
+++ src/apis/local.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   //baseURL: "http://localhost:9000/api/v1", //for the localhost
   //baseURL: "https://api.brightwayafrica.com/api/v1", // for production
-  baseURL: "http://3.225.222.48/api/v1", // for production
+  baseURL: "https://api.eshieldafrica.com/api/v1", // for production
 });
