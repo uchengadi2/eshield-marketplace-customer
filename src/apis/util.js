05555c68b64a330d52fac4a904f95bcb89f7d53e
Uche Ngadi <uchengadi@yahoo.com>
2022-11-08 15:30:09 +0100

    repointing the api url to production

--- src/apis/util.js
+++ src/apis/util.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:9000/";
-//export const baseURL = "https://api.eshieldafrica.com/";
+//export const baseURL = "http://localhost:9000/";
+export const baseURL = "https://api.eshieldafrica.com/";
